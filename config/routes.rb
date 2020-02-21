98a0a5d20a46f01d58620304d937d2253a2bf56d
Amanda Bezerra <colmenero88@bol.com.br>
2020-02-21 14:10:29 -0300

    fix webpack config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
-  resources :doses
-  resources :ingredients
-resources :cocktails, only: [] do
-  resources :doses
-end
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
