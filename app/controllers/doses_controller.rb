cfb896dc4a5df047e8a59e5e113838ea1ec22656
Amanda Bezerra <colmenero88@bol.com.br>
2020-02-21 09:13:33 -0300

    under construction

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:dose).permit(:description)
+  end
+end
